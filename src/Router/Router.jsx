fe98fc0b43b36da8151146839ac43119f51f9258
Igwe Naza <igweemmauelchina.gmail.com>
2025-12-27 17:59:38 +0100

    goof

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,32 +1,25 @@
-
-import {  Routes, Route } from "react-router-dom";
-
-
+import { Routes, Route, Navigate } from "react-router-dom";
 import Display from "../Components/Display";
 import About from "../Components/About";
 import Servicereadmore from "../Components/Servicereadmore";
 import RouteLoader from "../Components/Routeloader";
 import ContactPage from "../Components/Contacts";
 import Companyserveds from "../Components/Companyserveds";
-
-
-
-
+// Optional: Create a simple 404 component
+const NotFound = () => <div>404 - Page Not Found</div>;
 
 function Approuter() {
   return (
-<RouteLoader>
-<Routes>
-<Route path="/" element={<Display />} />
-<Route path="/Services" element={<Servicereadmore />} />
-<Route path="/About" element={<About />} />
-<Route path="/Contact" element={<ContactPage />} />
-<Route path="/Industries" element={<Companyserveds />} />
-</Routes>
-</RouteLoader>
+    <RouteLoader>
+      <Routes>
+        <Route path="/" element={<Display />} />
+        <Route path="/Services" element={<Servicereadmore />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<ContactPage />} />
+        <Route path="/Industries" element={<Companyserveds />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </RouteLoader>
   );
 }
 export default Approuter;
-
-
- 
